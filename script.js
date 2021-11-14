b7c857fbeea3587cd681c2e4b590eb9b45ef3b0d
vietdung1482000 <vietdung1482000@gmail.com>
2021-11-14 15:49:21 +0700

    da update hom nay

--- script.js
+++ script.js
@@ -1,58 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $('#menu').click(function(){
+  $('#menu').click(function () {
     $(this).toggleClass('fa-times');
     $('.navbar').toggleClass('nav-toggle');
   });
 
-  $(window).on('load scroll',function(){
+  $(window).on('load scroll', function () {
 
     $('#menu').removeClass('fa-times');
     $('.navbar').removeClass('nav-toggle');
 
-    if($(window).scrollTop() > 0){
+    if ($(window).scrollTop() > 0) {
       $('header').addClass('sticky');
-    }else{
+    } else {
       $('header').removeClass('sticky');
     }
 
-    if($(window).scrollTop() > 0){
+    if ($(window).scrollTop() > 0) {
       $('.scroll-top').show();
-    }else{
+    } else {
       $('.scroll-top').hide();
     }
 
     // scroll spy 
-
-    $('section').each(function(){
-
+    $('section').each(function () {
       let top = $(window).scrollTop();
       let offset = $(this).offset().top - 200;
       let id = $(this).attr('id');
       let height = $(this).height();
 
-      if(top > offset && top < offset + height){
+      if (top > offset && top < offset + height) {
         $('.navbar a').removeClass('active');
         $('.navbar').find(`[href="#${id}"]`).addClass('active');
       }
-
     });
-
   });
 
   // smooth scrolling 
-
-  $('a[href*="#"]').on('click',function(e){
-
+  $('a[href*="#"]').on('click', function (e) {
     $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
+      scrollTop: $($(this).attr('href')).offset().top,
     },
       500,
       'linear'
     );
-
   });
-
 });
